4b4f86f82767f0972e5e2d75bba4c1ae54f75bdb
Helena <helenaoliveira@alu.ufc.br>
2022-05-09 10:00:20 -0300

    Initial Commit

---
+++ MyFirstAppChallenge/MyFirstAppChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppChallengeApp.swift
+//  MyFirstAppChallenge
+//
+//  Created by Helena Oliveira on 09/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
